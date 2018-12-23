25c52576b54b73047737c4e42c26652f42d2e79a
Federico del Mazo <fdelmazo@fi.uba.ar>
2018-12-23 14:54:09 -0300

    Grafo jerarquico. Clusters para orientaciones y materias electivas

--- data/pdfToCSV.py
+++ data/pdfToCSV.py
@@ -19,7 +19,7 @@ def parsePDF(f):
         wholeText = wholeText.replace('\n-\n','-')
     return wholeText
 
-def materias(filename, categoria, wholeText, start, end=None, fields=5, discard=[3]):
+def materias(filename, wholeText, categoria, nivel_en_grafo, start, end=None, fields=5, discard=[3]):
     ASIGNATURAS = []
     if end: asignaturas = wholeText[wholeText.find(start)+len(start):wholeText.find(end,wholeText.find(start))]
     else: asignaturas = wholeText[wholeText.find(start)+len(start):]
@@ -34,7 +34,7 @@ def materias(filename, categoria, wholeText, start, end=None, fields=5, discard=
 
     for i,p in enumerate(parrafos):
         if CODIGO.search(p):
-            asignaturas.append(tuple(parrafos[i:i+fields]))
+            asignaturas.append(parrafos[i:i+fields])
             for j in range(i,i+fields): parrafos[j] = ''
 
     asignaturas_clean = []
@@ -43,15 +43,13 @@ def materias(filename, categoria, wholeText, start, end=None, fields=5, discard=
         asignatura_clean = []
         for x in a:
             if discard and a.index(x) in discard: continue
+            if x=='140 créditos aprobados' or x=='140 créditos': x='140'
             asignatura_clean.append(x.replace('\n','').replace(' (*)','').strip())
-        asignaturas_clean.append(tuple([*asignatura_clean,categoria]))
+        asignaturas_clean.append(tuple([*asignatura_clean,categoria,nivel_en_grafo]))
         
     ASIGNATURAS.extend(asignaturas_clean)
 
     ASIGNATURAS = [a for a in ASIGNATURAS if not a[3] == 'continúa']
-
-    for a in ASIGNATURAS:
-        assert(len(a)==5)
 
     with open(filename, 'a') as f:
         wr = csv.writer(f)
@@ -64,18 +62,23 @@ def main():
     
     with open(filename, 'w') as f:
         wr = csv.writer(f)
-        wr.writerow(['codigo', 'materia','creditos','correlativas','categoria'])
-        wr.writerow(['CBC', 'Ciclo Básico Común','0',' ','Ciclo Básico Común'])
-        wr.writerow(['75.61', 'Taller de Programación III','6','66.74-75.74','Orientacion: Gestion Industrial de Sistemas (opción tesis)']) #Caso muuuuy particular
+        wr.writerow(['codigo', 'materia','creditos','correlativas','categoria','nivel_en_grafo'])
+        wr.writerow(['CBC', 'Ciclo Básico Común','0','x','Ciclo Básico Común','0'])
+        wr.writerow(['140', '140 créditos  aprobados','0','CBC','xx','9'])
 
+        wr.writerow(['71.40', 'Legislación y Ejercicio Profesional de la Ingeniería en Informática','4','140','xxx','10'])
+        wr.writerow(['75.00', 'Tesis','12','71.40','Opción Tesis','11'])
+        wr.writerow(['75.99', 'Trabajo Profesional','6','71.40','Opción Trabajo Profesional','11'])
 
-    materias(filename, 'Materias Obligatorias',wholeText, 'DISTRIBUCION DE ASIGNATURAS','ORIENTACION')
-    materias(filename, 'Materias Electivas',wholeText, 'ASIGNATURAS ELECTIVAS','78.01',fields=4,discard=[])
-    materias(filename, 'Idiomas',wholeText, 'Idioma Inglés','Solo se tendrá',fields=4,discard=[])
-    materias(filename, 'Orientacion: Gestión Industrial de Sistemas',wholeText, 'ORIENTACION EN GESTION INDUSTRIAL DE SISTEMAS', 'OPCION TESIS')
-    materias(filename, 'Orientacion: Sistemas Distribuidos',wholeText, 'ORIENTACION EN SISTEMAS DISTRIBUIDOS', 'OPCION TESIS')
-    materias(filename, 'Orientacion: Sistemas de Producción',wholeText, 'ORIENTACION EN SISTEMAS DE PRODUCCION', 'OPCION TESIS')
-    materias(filename, 'Opción Tésis',wholeText, 'OPCION TESIS DE INGENIERIA EN INFORMATICA', 'OPCION TRABAJO PROFESIONAL DE INGENIERIA EN INFORMATICA')
-    materias(filename, 'Opción Trabajo Profesional',wholeText, 'OPCION TRABAJO PROFESIONAL DE INGENIERIA EN INFORMATICA', 'TOTAL DE LA CARRERA (CBC + SEGUNDO CICLO)')    
+    materias(filename, wholeText, 'Materias Obligatorias', 1, 'TERCER CUATRIMESTRE','CUARTO CUATRIMESTRE')
+    materias(filename, wholeText, 'Materias Obligatorias', 2, 'CUARTO CUATRIMESTRE','QUINTO CUATRIMESTRE')
+    materias(filename, wholeText, 'Materias Obligatorias', 3, 'QUINTO CUATRIMESTRE','SEXTO CUATRIMESTRE')
+    materias(filename, wholeText, 'Materias Obligatorias', 4, 'SEXTO CUATRIMESTRE','SEPTIMO CUATRIMESTRE')
+    materias(filename, wholeText, 'Materias Obligatorias', 5, 'SEPTIMO CUATRIMESTRE','OCTAVO CUATRIMESTRE')
+    materias(filename, wholeText, 'Materias Obligatorias', 6, 'OCTAVO CUATRIMESTRE','ORIENTACION EN')
+    materias(filename, wholeText, 'Materias Electivas', 7, 'ASIGNATURAS ELECTIVAS',fields=4,discard=[])
+    materias(filename, wholeText, 'Orientacion: Gestión Industrial de Sistemas', 8, 'ORIENTACION EN GESTION INDUSTRIAL DE SISTEMAS', 'OPCION TESIS')
+    materias(filename, wholeText, 'Orientacion: Sistemas Distribuidos', 8, 'ORIENTACION EN SISTEMAS DISTRIBUIDOS', 'OPCION TESIS')
+    materias(filename, wholeText, 'Orientacion: Sistemas de Producción', 8, 'ORIENTACION EN SISTEMAS DE PRODUCCION', 'OPCION TESIS')
 
 main()
