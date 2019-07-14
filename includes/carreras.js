b56d4e7b45282808c521fa615fa2bde2c05458d3
Federico del Mazo <fdelmazo@fi.uba.ar>
2019-07-14 10:14:25 -0300

    Actualiza titulos de planes

--- includes/carreras.js
+++ includes/carreras.js
@@ -7,67 +7,67 @@ function update(element, carrera){
             orientaciones = true
             filename = 'data/informatica-1986.csv'
             titulo = 'Ingeniería en Informática'
-            plan = 'Plan 1986'
+            plan = 'Plan 1986 v2016'
             break
         case 'sistemas':
             orientaciones = false
             filename = 'data/sistemas-1986.csv'
             titulo = 'Licenciatura en Análisis de Sistemas'
-            plan = 'Plan 1986'
+            plan = 'Plan 1986 v2016'
             break
         case 'electronica':
             orientaciones = true
             filename = 'data/electronica-2009.csv'
             titulo = 'Ingeniería Electrónica'
-            plan = 'Plan 2009'
+            plan = 'Plan 2009 v2019'
             break
         case 'quimica':
             orientaciones = false
             filename = 'data/quimica-1986.csv'
             titulo = 'Ingeniería Química'
-            plan = 'Plan 1986'
+            plan = 'Plan 1986 v2016'
             break
         case 'civil':
             orientaciones = false
             filename = 'data/civil-2009.csv'
             titulo = 'Ingeniería Civil'
-            plan = 'Plan 2009'
+            plan = 'Plan 2009 v2016'
             break
         case 'alimentos':
             orientaciones = false
             filename = 'data/alimentos-2000.csv'
             titulo = 'Ingeniería de Alimentos'
-            plan = 'Plan 2000'
+            plan = 'Plan 2000 v2016'
             break
         case 'electricista':
             orientaciones = false
             filename = 'data/electricista-2009.csv'
             titulo = 'Ingeniería Electricista'
-            plan = 'Plan 2009'
+            plan = 'Plan 2009 v2016'
             break
         case 'naval':
             orientaciones = false
             filename = 'data/naval-1986.csv'
             titulo = 'Ingeniería Naval y Mecánica'
-            plan = 'Plan 1986'
+            plan = 'Plan 1986 v2016'
             break
         case 'mecanica':
             orientaciones = true
             filename = 'data/mecanica-1986.csv'
             titulo = 'Ingeniería Mecánica '
-            plan = 'Plan 1986'
+            plan = 'Plan 1986 v2016'
             break
         case 'petroleo':
             orientaciones = false
             filename = 'data/petroleo-2015.csv'
             titulo = 'Ingeniería en Petróleo'
-            plan = 'Plan 2015'
+            plan = 'Plan 2015 v2016'
             break
         case 'agrimensura':
             orientaciones = false
             filename = 'data/agrimensura-2006.csv'
             titulo = 'Ingeniería en Argimensura'
-            plan = 'Plan 2006'
+            plan = 'Plan 2006 v2016'
             break
     }
 
@@ -76,7 +76,6 @@ function update(element, carrera){
     $("#carrera-actual").text(titulo + ' | ' + plan)
     $("#carreras .active").removeClass('active')
     $(element).addClass('active')
-    document.title = 'FIUBA Map | ' + titulo
     displayOrientaciones(orientaciones)
 }
 
