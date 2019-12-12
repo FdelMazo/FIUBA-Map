b11e3b913c502f338bb60c229fc200c395cf0c9a
FdelMazo <fdelmazo@fi.uba.ar>
2019-12-12 13:03:11 -0300

    Agrega cartel de ayuda para todas las carreras

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function main(carrera) {
     $('.dropdown-content').hide();
     $("#grafo").html("<div class='loader'></div>");
-    let filename, titulo, tituloShort, orientaciones, plan;
+    let filename, titulo, tituloShort, orientaciones, plan, help;
     switch (carrera) {
         case 'informatica':
             orientaciones = true;
@@ -9,6 +9,10 @@ function main(carrera) {
             plan = 'Plan 1986 v2016';
             titulo = 'Ingeniería en Informática';
             tituloShort = 'Informática';
+            help = `<li>156 créditos de materias obligatorias</li>
+                    <li>34 créditos de una orientación</li>
+                    <li>Tesis ó Trabajo Profesional</li>
+                    <li>34 créditos de electivas (Tésis) ó 46 créditos de electivas (TPP)</li>`
             break;
         case 'sistemas':
             orientaciones = false;
@@ -16,6 +20,9 @@ function main(carrera) {
             plan = 'Plan 1986 v2016';
             titulo = 'Licenciatura en Análisis de Sistemas';
             tituloShort = 'Sistemas';
+            help = `<li>148 créditos de materias obligatorias</li>
+                    <li>28 créditos de electivas</li>
+                    <li>Inglés</li>`
             break;
         case 'electronica':
             orientaciones = true;
@@ -23,6 +30,11 @@ function main(carrera) {
             plan = 'Plan 2009 v2019';
             titulo = 'Ingeniería Electrónica';
             tituloShort = 'Electrónica';
+            help = `<li>166 créditos de materias obligatorias</li>
+                    <li>56 créditos de electivas</li>
+                    <li>Inglés</li>
+                    <li>Tesis ó Trabajo Profesional</li>
+                    <li>Práctica Profesional</li>`
             break;
         case 'quimica':
             orientaciones = false;
@@ -30,6 +42,9 @@ function main(carrera) {
             plan = 'Plan 1986 v2016';
             titulo = 'Ingeniería Química';
             tituloShort = 'Química';
+            help = `<li>216 créditos de materias obligatorias</li>
+                    <li>Tesis ó Trabajo Profesional</li>
+                    <li>18 créditos de electivas (Tésis) ó 24 créditos de electivas (TPP)</li>`
             break;
         case 'civil':
             orientaciones = false;
@@ -37,6 +52,11 @@ function main(carrera) {
             plan = 'Plan 2009 v2016';
             titulo = 'Ingeniería Civil';
             tituloShort = 'Civil';
+            help = `<li>210 créditos de materias obligatorias</li>
+                    <li>34 créditos de electivas</li>
+                    <li>Inglés</li>
+                    <li>Trabajo Profesional</li>
+                    <li>Práctica Profesional</li>`
             break;
         case 'alimentos':
             orientaciones = false;
@@ -44,6 +64,11 @@ function main(carrera) {
             plan = 'Plan 2000 v2016';
             titulo = 'Ingeniería de Alimentos';
             tituloShort = 'Alimentos';
+            help = `<li>210 créditos de materias obligatorias</li>
+                    <li>34 créditos de electivas</li>
+                    <li>Inglés</li>
+                    <li>Trabajo Profesional</li>
+                    <li>Práctica Profesional</li>`
             break;
         case 'electricista':
             orientaciones = false;
@@ -51,6 +76,11 @@ function main(carrera) {
             plan = 'Plan 2009 v2016';
             titulo = 'Ingeniería Electricista';
             tituloShort = 'Electricista';
+            help = `<li>206 créditos de materias obligatorias</li>
+                    <li>Inglés</li>
+                    <li>Tesis ó Trabajo Profesional</li>
+                    <li>Práctica Profesional</li>
+                    <li>16 créditos de electivas (Tésis) ó 22 créditos de electivas (TPP)</li>`
             break;
         case 'naval':
             orientaciones = false;
@@ -58,6 +88,9 @@ function main(carrera) {
             plan = 'Plan 1986 v2016';
             titulo = 'Ingeniería Naval y Mecánica';
             tituloShort = 'Naval';
+            help = `<li>226 créditos de materias obligatorias</li>
+                    <li>Tesis ó Trabajo Profesional</li>
+                    <li>20 créditos de electivas (Tésis) ó 38 créditos de electivas (TPP)</li>`
             break;
         case 'mecanica':
             orientaciones = true;
@@ -65,6 +98,10 @@ function main(carrera) {
             plan = 'Plan 1986 v2016';
             titulo = 'Ingeniería Mecánica';
             tituloShort = 'Mecánica';
+            help = `<li>190 créditos de materias obligatorias</li>
+                    <li>28 créditos de una orientación</li>
+                    <li>Tesis ó Trabajo Profesional</li>
+                    <li>24 créditos de electivas (Tésis) ó 28 créditos de electivas (TPP)</li>`
             break;
         case 'petroleo':
             orientaciones = false;
@@ -72,21 +109,36 @@ function main(carrera) {
             plan = 'Plan 2015 v2016';
             titulo = 'Ingeniería en Petróleo';
             tituloShort = 'Petróleo';
-            break;
+            help = `<li>216 créditos de materias obligatorias</li>
+                    <li>Inglés</li>
+                    <li>Tesis ó Trabajo Profesional</li>
+                    <li>Práctica Profesional</li>
+                    <li>12 créditos de electivas (Tésis) ó 16 créditos de electivas (TPP)</li>`
+            break;        
         case 'agrimensura':
             orientaciones = false;
             filename = 'data/agrimensura-2006.csv';
             plan = 'Plan 2006 v2016';
             titulo = 'Ingeniería en Argimensura';
             tituloShort = 'Agrimensura';
-            break;
+            help = `<li>178 créditos de materias obligatorias</li>
+                    <li>Tesis ó Trabajo Profesional</li>
+                    <li>Práctica Profesional</li>
+                    <li>12 créditos de electivas (Tésis) ó 18 créditos de electivas (TPP)</li>`
+            break;        
         case 'industrial':
             orientaciones = false;
             filename = 'data/industrial-2011.csv';
             plan = 'Plan 2011 v2019';
             titulo = 'Ingeniería Industrial';
             tituloShort = 'Industrial';
-            break
+            help = `<li>196 créditos de materias obligatorias</li>
+                    <li>32 créditos de electivas</li>
+                    <li>4 créditos de materias humanísticas de otras facultades de la UBA</li>
+                    <li>Ingles</li>
+                    <li>Tesis ó Trabajo Profesional</li>
+                    <li>Práctica Profesional</li>`
+            break;        
     }
 
     $("#carrera-actual-long").text(titulo + ' | ' + plan);
@@ -95,6 +147,7 @@ function main(carrera) {
     $("#" + carrera).addClass('active');
     $("#orientaciones a").remove();
     $('#creditos-var').text(0);
+    $('#help-list').html(help);
     $('#promedio-var').text('-');
     $("[id='toggle-Materias Electivas']").css("background-color", "");
     if (orientaciones) $("#orientaciones-hidden").show();
