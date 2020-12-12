1bde3d075dea1e879091c1fc16c660c12e87e690
FdelMazo <fdelmazo@fi.uba.ar>
2020-12-12 19:53:11 -0300

    Add every carrera json to data dir

--- src/data/index.js
+++ src/data/index.js
@@ -1,2 +1,12 @@
-export { informatica } from "./informatica-1986.js";
-export { sistemas } from "./sistemas-1986.js";
+export { agrimensura } from "./agrimensura-2006";
+export { alimentos } from "./alimentos-2000";
+export { civil } from "./civil-2009";
+export { electricista } from "./electricista-2009";
+export { electronica } from "./electronica-2009";
+export { industrial } from "./industrial-2011";
+export { informatica } from "./informatica-1986";
+export { mecanica } from "./mecanica-1986";
+export { naval } from "./naval-1986";
+export { petroleo } from "./petroleo-2015";
+export { quimica } from "./quimica-1986";
+export { sistemas } from "./sistemas-1986";
