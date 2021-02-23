4ffa57563aa65540a9a4f3f50935f88cf228faa4
FdelMazo <fdelmazo@fi.uba.ar>
2021-02-22 23:59:13 -0300

    Add frontend for save/autosave feature

--- src/components/PadronInput.js
+++ src/components/PadronInput.js
@@ -1,5 +1,19 @@
 import { CheckCircleIcon, SettingsIcon } from "@chakra-ui/icons";
-import { Box, Button, Flex, Input, useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Icon,
+  IconButton,
+  Input,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+  Switch,
+  useDisclosure,
+} from "@chakra-ui/react";
 import React from "react";
 import { UserContext } from "../Contexts";
 import UserModal from "./UserModal";
@@ -60,6 +74,40 @@ const PadronInput = () => {
               <CheckCircleIcon />
             )}
           </Button>
+          {logged && (
+            <Popover placement="right-end" trigger="hover">
+              <PopoverTrigger>
+                <IconButton
+                  colorScheme={"teal"}
+                  variant={"outline"}
+                  size="sm"
+                  isLoading={loading} //saving
+                  onClick={() => {
+                    alert("Im saving!");
+                  }}
+                >
+                  <Icon boxSize={5} viewBox="0 0 448 512" color="black">
+                    <path
+                      fill="teal"
+                      d="M433.941 129.941l-83.882-83.882A48 48 0 0 0 316.118 32H48C21.49 32 0 53.49 0 80v352c0 26.51 21.49 48 48 48h352c26.51 0 48-21.49 48-48V163.882a48 48 0 0 0-14.059-33.941zM224 416c-35.346 0-64-28.654-64-64 0-35.346 28.654-64 64-64s64 28.654 64 64c0 35.346-28.654 64-64 64zm96-304.52V212c0 6.627-5.373 12-12 12H76c-6.627 0-12-5.373-12-12V108c0-6.627 5.373-12 12-12h228.52c3.183 0 6.235 1.264 8.485 3.515l3.48 3.48A11.996 11.996 0 0 1 320 111.48z"
+                    />
+                  </Icon>
+                </IconButton>
+              </PopoverTrigger>
+              <PopoverContent size="sm" w="fit-content" borderColor="black">
+                <PopoverArrow />
+                <PopoverBody
+                  onClick={() => {
+                    alert("Im autosaving!");
+                  }}
+                  color="black"
+                >
+                  auto-save
+                  <Switch colorScheme="teal" ml={2} />
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
+          )}
         </Flex>
       </form>
       <UserModal padronInput={padronInput} onClose={onClose} isOpen={isOpen} />
