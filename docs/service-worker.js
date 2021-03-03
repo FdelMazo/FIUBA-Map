d6134ccb729c1445fd03f33ce772b0a39afec980
FdelMazo <fdelmazo@fi.uba.ar>
2021-03-03 14:22:36 +0000

    Builds site w/gh actions [auto]

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FIUBA-Map/precache-manifest.c3bd7297ed8b43fce82ecda3860d96f3.js"
+  "/FIUBA-Map/precache-manifest.cfdb31ba8f12ffa89c8ebd8302b620f2.js"
 );
 
 self.addEventListener('message', (event) => {
