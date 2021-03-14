53c7ea8f9cc695c1d9360025a6175c1df20c82cd
FdelMazo <fdelmazo@fi.uba.ar>
2021-03-14 13:37:35 -0300

    Change carrera select for chakra menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,14 @@
-import { Flex, ScaleFade, Select, useColorModeValue } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import {
+  Flex,
+  Menu,
+  MenuButton,
+  MenuItemOption,
+  MenuList,
+  MenuOptionGroup,
+  ScaleFade,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import React from "react";
 import CARRERAS from "../carreras";
 import { GraphContext, UserContext } from "../Contexts";
@@ -25,23 +35,38 @@ const Header = (props) => {
       </ScaleFade>
 
       <ScaleFade in={!displayedNode} unmountOnExit>
-        <Select
-          borderColor="white"
-          color="white"
-          key={user.carrera?.id}
-          css={{
-            "& *": { color: "initial" },
-          }}
-          onChange={(e) => {
-            setFirstTime(false);
-            changeCarrera(e.target.value);
-          }}
-          value={user.carrera?.id}
-        >
-          {CARRERAS.map((c) => (
-            <option value={c.id}>{c.nombre}</option>
-          ))}
-        </Select>
+        <Menu placement="bottom-end" closeOnSelect={false}>
+          <MenuButton
+            _hover={{ borderColor: "gray.400" }}
+            _expanded={{ borderColor: "blue.400" }}
+            _focus={{ borderColor: "blue.400" }}
+            px={4}
+            py={2}
+            color="white"
+            borderColor="white"
+            borderRadius="md"
+            borderWidth="1px"
+          >
+            {user.carrera?.nombre}
+            <ChevronDownIcon ml={2} />
+          </MenuButton>
+
+          <MenuList>
+            <MenuOptionGroup
+              onChange={(v) => {
+                setFirstTime(false);
+                changeCarrera(v);
+              }}
+              key={user.carrera?.id}
+              defaultValue={user.carrera?.id}
+              type="radio"
+            >
+              {CARRERAS.map((c) => (
+                <MenuItemOption value={c.id}>{c.nombre}</MenuItemOption>
+              ))}
+            </MenuOptionGroup>
+          </MenuList>
+        </Menu>
       </ScaleFade>
     </Flex>
   );
