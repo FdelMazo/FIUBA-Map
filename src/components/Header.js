77046ae0716af925d72f4adf2f2f64ef62c8aae0
FdelMazo <fdelmazo@fi.uba.ar>
2021-12-07 12:43:25 -0300

    Avoid tooltip to overflow page (on some browsers)

--- src/components/Header.js
+++ src/components/Header.js
@@ -73,7 +73,7 @@ const Header = (props) => {
           </MenuList>
         </Menu>
         {!mobile && (
-          <Tooltip label="Plan de Estudios" placement="bottom">
+          <Tooltip label="Plan de Estudios" placement="bottom-start">
             <Link color="white" href={user.carrera?.link} isExternal>
               <Icon boxSize={6} ml={2} viewBox="0 0 512 512">
                 <path
