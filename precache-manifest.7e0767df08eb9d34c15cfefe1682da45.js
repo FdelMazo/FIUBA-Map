122faa04f6384d48270f94f1e330f5dc4e14d7eb
FdelMazo <fdelmazo@fi.uba.ar>
2022-09-12 22:20:14 +0000

    Deploying to gh-pages from @ FdelMazo/FIUBA-Map@dbcf771cdf451ee102aa5a500fd2edcb64623d6a 🚀

---
+++ precache-manifest.7e0767df08eb9d34c15cfefe1682da45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df489c8ececf8d34f399a2713f3c96b5",
+    "url": "/FIUBA-Map/index.html"
+  },
+  {
+    "revision": "214e9c2294ddf3c4a42e",
+    "url": "/FIUBA-Map/static/js/2.b5632a9d.chunk.js"
+  },
+  {
+    "revision": "b45e14205663e3a520b3e0a455bba542",
+    "url": "/FIUBA-Map/static/js/2.b5632a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77fdf889ab309237a670",
+    "url": "/FIUBA-Map/static/js/main.010b62cc.chunk.js"
+  },
+  {
+    "revision": "15f20f19a13ea8d2a6c8",
+    "url": "/FIUBA-Map/static/js/runtime-main.9b100fd5.js"
+  }
+]);
