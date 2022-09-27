0b50ac8a7c677bd904618890df9b67f57eb819bd
FdelMazo <FdelMazo@fi.uba.ar>
2022-09-26 22:04:14 -0300

    Build refactored site

---
+++ docs/precache-manifest.d3102059629c12c08e7eaf9d2d861a35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca56c11622af966d5a92eb26700be5f",
+    "url": "/FIUBA-Map/index.html"
+  },
+  {
+    "revision": "81eb6fbdc033d7575c87",
+    "url": "/FIUBA-Map/static/js/2.9fd579af.chunk.js"
+  },
+  {
+    "revision": "b45e14205663e3a520b3e0a455bba542",
+    "url": "/FIUBA-Map/static/js/2.9fd579af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d74568f7e54cdea3a2ee",
+    "url": "/FIUBA-Map/static/js/main.f5090a29.chunk.js"
+  },
+  {
+    "revision": "15f20f19a13ea8d2a6c8",
+    "url": "/FIUBA-Map/static/js/runtime-main.9b100fd5.js"
+  }
+]);
