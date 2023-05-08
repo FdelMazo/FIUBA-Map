7cb2be48ee8005a60a254624d8517557936b7093
Federico del Mazo <FdelMazo@fi.uba.ar>
2023-05-08 12:31:12 -0300

    Arrancan los disclaimers

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,8 @@
 import {
   ScaleFade,
   Flex,
+  Alert,
+  Link,
   Box,
   useColorModeValue,
 } from "@chakra-ui/react";
@@ -13,9 +15,9 @@ import UserMenu from "./UserMenu";
 
 
 const Header = () => {
-  const { isMobile, logged } = React.useContext(UserContext);
+  const { isMobile, logged, user } = React.useContext(UserContext);
   const { displayedNode } = React.useContext(GraphContext);
-
+  const isInformatica = user?.carrera?.id === "informatica";
   const CommonProps = {
     height: isMobile ? "8.7rem" : "4.5rem",
     bg: useColorModeValue("headerbg", "headerbgdark")
@@ -51,6 +53,17 @@ const Header = () => {
           ) : (
             <PadronInput />
           )}
+          <ScaleFade in={isInformatica}>
+            <Alert colorScheme='purple' borderRadius="full">
+              <Link
+                isExternal
+                href="https://youtu.be/I0AxrOUJ62E"
+              >
+                Trabajando duramente en el plan 2020.
+
+              </Link>
+            </Alert>
+          </ScaleFade>
           <DropdownCarreras />
         </Flex>
       </ScaleFade>
